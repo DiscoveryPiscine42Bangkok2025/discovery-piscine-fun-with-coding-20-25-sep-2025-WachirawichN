5e2dd21b31fa0b4bd714b08f14a4b6d32885cee0
Page <wnamgird@gmail.com>
2025-09-24 03:53:08 +0700

    finish home page

--- rush/home/home.js
+++ rush/home/home.js
@@ -1,35 +1,58 @@
 const jaiBox = document.getElementById("jaiBox");
 const champBox = document.getElementById("champBox");
+const loadBox = document.getElementById("loadBox");
 
-function mouseEnterBox(event)
-{
-    const image = event.target.querySelector("img");
-    const overlay = event.target.querySelector(".colorOverlay");
-    const nameText = event.target.querySelector(".nameTitle");
+let currentBox = 1;
 
-    image.style.opacity = "100%";
-    overlay.style.opacity = "50%";
-    nameText.style.opacity = "100%";
+function mouseIn() {
+    jaiBox.querySelector("button").style.backgroundColor = "rgba(119, 181, 27, 1)";
 }
-function mouseLeaveBox(event)
-{
-    const image = event.target.querySelector("img");
-    const overlay = event.target.querySelector(".colorOverlay");
-    const nameText = event.target.querySelector(".nameTitle");
-    
-    image.style.opacity = "20%";
-    overlay.style.opacity = "10%";
-    nameText.style.opacity = "0%";
+function mouseOut() {
+    jaiBox.querySelector("button").style.backgroundColor = "rgb(109, 168, 19)";
 }
 
-jaiBox.addEventListener("click", function() {
+jaiBox.querySelector("button").addEventListener("click", function() {
+    console.log("Nigga");
     window.location.href = "../wnamgird/wnamgird.html"
 });
-jaiBox.addEventListener("mouseenter", mouseEnterBox);
-jaiBox.addEventListener("mouseleave", mouseLeaveBox);
+jaiBox.querySelector("button").addEventListener("mouseenter", mouseIn);
+jaiBox.querySelector("button").addEventListener("mouseleave", mouseOut);
 
-champBox.addEventListener("click", function() {
+champBox.querySelector("button").addEventListener("click", function() {
     window.location.href = "../csuwanpi/csuwanpi.html"
 });
-champBox.addEventListener("mouseenter", mouseEnterBox);
-champBox.addEventListener("mouseleave", mouseLeaveBox);
+champBox.querySelector("button").addEventListener("mouseenter", mouseIn);
+champBox.querySelector("button").addEventListener("mouseleave", mouseOut);
+
+function loadBarSwitch() {
+    if (currentBox == 0) {
+        jaiBox.style.opacity = "0%";
+        jaiBox.style.zIndex = 0;
+        champBox.style.opacity = "100%";
+        champBox.style.zIndex = 1;
+
+        loadBox.querySelector("#jaiLoadBox").querySelector(".runBar").style.opacity = "0%";
+        loadBox.querySelector("#jaiLoadBox").querySelector(".runBar").style.width = "0%";
+        loadBox.querySelector("#champLoadBox").querySelector(".runBar").style.opacity = "100%";
+        loadBox.querySelector("#champLoadBox").querySelector(".runBar").style.width = "100%";
+        
+        currentBox = 1;
+    }
+    else {
+        jaiBox.style.opacity = "100%";
+        jaiBox.style.zIndex = 1;
+        champBox.style.opacity = "0%";
+        champBox.style.zIndex = 0;
+
+        loadBox.querySelector("#jaiLoadBox").querySelector(".runBar").style.opacity = "100%";
+        loadBox.querySelector("#jaiLoadBox").querySelector(".runBar").style.width = "100%";
+        loadBox.querySelector("#champLoadBox").querySelector(".runBar").style.opacity = "0%";
+        loadBox.querySelector("#champLoadBox").querySelector(".runBar").style.width = "0%";
+        
+        currentBox = 0;
+    }
+}
+
+loadBarSwitch();
+loadBox.querySelector("#jaiLoadBox").querySelector(".runBar").addEventListener("transitionend", loadBarSwitch);
+loadBox.querySelector("#champLoadBox").querySelector(".runBar").addEventListener("transitionend", loadBarSwitch);
