bb70f0a889f4b72210845a0f2a9fe470d7002221
Page <wnamgird@gmail.com>
2025-09-24 11:08:45 +0700

    refine home page

--- rush/home/home.js
+++ rush/home/home.js
@@ -25,14 +25,18 @@ champBox.querySelector("button").addEventListener("mouseenter", mouseIn);
 champBox.querySelector("button").addEventListener("mouseleave", mouseOut);
 
 function loadBarSwitch() {
+    console.log(currentBox);
     if (currentBox == 0) {
         jaiBox.style.opacity = "0%";
         jaiBox.style.zIndex = 0;
         champBox.style.opacity = "100%";
         champBox.style.zIndex = 1;
 
+        loadBox.querySelector("#jaiLoadBox").querySelector(".runBar").style.transition = "width 0s linear";
         loadBox.querySelector("#jaiLoadBox").querySelector(".runBar").style.opacity = "0%";
         loadBox.querySelector("#jaiLoadBox").querySelector(".runBar").style.width = "0%";
+        
+        loadBox.querySelector("#champLoadBox").querySelector(".runBar").style.transition = "width 10s linear";
         loadBox.querySelector("#champLoadBox").querySelector(".runBar").style.opacity = "100%";
         loadBox.querySelector("#champLoadBox").querySelector(".runBar").style.width = "100%";
         
@@ -44,8 +48,11 @@ function loadBarSwitch() {
         champBox.style.opacity = "0%";
         champBox.style.zIndex = 0;
 
+        loadBox.querySelector("#jaiLoadBox").querySelector(".runBar").style.transition = "width 10s linear";
         loadBox.querySelector("#jaiLoadBox").querySelector(".runBar").style.opacity = "100%";
         loadBox.querySelector("#jaiLoadBox").querySelector(".runBar").style.width = "100%";
+        
+        loadBox.querySelector("#champLoadBox").querySelector(".runBar").style.transition = "width 0s linear";
         loadBox.querySelector("#champLoadBox").querySelector(".runBar").style.opacity = "0%";
         loadBox.querySelector("#champLoadBox").querySelector(".runBar").style.width = "0%";
         
@@ -53,6 +60,9 @@ function loadBarSwitch() {
     }
 }
 
-loadBarSwitch();
+loadBox.querySelector("#jaiLoadBox").querySelector(".hitBox").addEventListener("click", loadBarSwitch);
 loadBox.querySelector("#jaiLoadBox").querySelector(".runBar").addEventListener("transitionend", loadBarSwitch);
+loadBox.querySelector("#champLoadBox").querySelector(".hitBox").addEventListener("click", loadBarSwitch);
 loadBox.querySelector("#champLoadBox").querySelector(".runBar").addEventListener("transitionend", loadBarSwitch);
+
+document.addEventListener("DOMContentLoaded", loadBarSwitch);
